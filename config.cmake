8645ab2d0b5f849ee3d0d3dc45bc387296d3a5ce
danicisneross <d.cisneross@udc.es>
2022-03-01 16:33:38 +0100

    plantilla p1

---
+++ config.cmake
@@ -0,0 +1,8 @@
+#Choose between DYNAMIC or STATIC to use a concrete implementation (uncomment desired line)
+#set(impl DYNAMIC)
+#set(impl STATIC)
+set(impl TESTLIST)
+
+
+#Choose the input file for main (new.txt, bid.txt, delete.txt...)
+set(input new.txt)
