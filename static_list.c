16124cf9bdd8d59afe851d992c3f6c1455989643
danicisneross <d.cisneross@udc.es>
2022-03-09 13:40:08 +0100

    statics

--- static_list.c
+++ static_list.c
@@ -8,5 +8,83 @@
  */
 
 #include "static_list.h"
+#include "string.h"
 
-/* Write your code here... */
+void createEmptyList(tList *L){
+    L->lastPos= LNULL;
+}
+
+bool isEmptyList(tList L){
+    return L.lastPos==LNULL;
+}
+
+tPosL first(tList L){
+    return 0;
+}
+
+tPosL last(tList L){
+    return L.lastPos;
+}
+
+tPosL next(tPosL p, tList L){
+    if (p==L.lastPos)
+        return LNULL;
+    else
+        return ++p;
+
+}
+
+tPosL previous(tPosL p, tList L){
+    if(p==0)
+        return LNULL;
+    else
+        return --p;
+}
+
+bool insertItem(tItemL d, tPosL p, tList *L){
+    tPosL i;
+
+    if (L->lastPos==MAX-1) //La lista esta llena
+        return false;
+    else {
+        L->lastPos++;
+        if (p==LNULL) { //Insertar al final
+            L->data[L->lastPos] = d;
+        } else {
+            for (i = L->lastPos; i >=p+1; i--)
+                L->data[i] = L->data[i-1];
+            L->data[p] = d;
+        }
+        return true;
+
+    }
+}
+
+void deleteAtPosition(tPosL p, tList *L){
+    tPosL i;
+
+    L->lastPos--;
+    for(i=p;i <=L->lastPos; i++)
+        L->data[i]= L->data[i+1];
+}
+
+tItemL getItem(tPosL p, tList L){
+    return L.data[p];
+}
+
+void updateItem(tItemL d, tPosL p, tList *L){
+    L->data[p]=d;
+}
+
+tPosL findItem(tProductId pd, tList L){
+    tPosL p;
+
+    if (L.lastPos==LNULL)
+        return LNULL;
+    else {
+        for(p=first(L); p<=L.lastPos ; p++){
+            if (strcmp (L.data[p].productId, pd)  == 0)
+                return p;
+        } return LNULL;
+    }
+}
