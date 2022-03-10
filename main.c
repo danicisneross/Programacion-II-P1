f13b282426f02714c725d272bc608753a003e9dc
danicisneross <d.cisneross@udc.es>
2022-03-10 19:24:27 +0100

    new y delete

--- main.c
+++ main.c
@@ -13,28 +13,84 @@
 #include "types.h"
 
 #define MAX_BUFFER 255
+#define N 20
 
 #ifdef DYNAMIC_LIST
+
 #include "dynamic_list.h"
+
 #endif
 #ifdef STATIC_LIST
 #include "static_list.h"
 #endif
 
+void new(tProductId pd, tUserId userId, tProductCategory prodCategory, tProductPrice price, tList *L) {
+    char category[N];
 
+    if (findItem(pd, *L) == LNULL) {
+        tItemL prod;
+        strcpy(prod.productId, pd);
+        prod.bidCounter = 0;
 
+        if (prodCategory == book) {
+            prod.productCategory = book;
+            strcpy(category, "book");
+        } else {
+            prod.productCategory = painting;
+            strcpy(category, "painting");
+        }
+        if (insertItem(prod, LNULL, L) == true) {
+            printf("* New: product %s seller %s category %s price %.2f \n", pd, userId, category, price);
+        } else {
+            printf("+ Error: New not possible");
+        }
+    } else {
+        printf("+ Error: New not possible");
+    }
+}
 
-void processCommand(char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4) {
+void delete(tProductId prodId, tList *L){
+    tPosL p;
+    tItemL prod;
+    char category[N];
+    p = findItem(prodId, *L);
 
+    if (prod.productCategory == book) {
+        strcpy(category, "book");
+    } else {
+        strcpy(category, "painting");
+    }
+
+    if(p != LNULL) {
+        prod = getItem(p, *L);
+        deleteAtPosition(p, L);
+        printf("* Delete: product %s seller %s category %s price %.2f bids %d\n", prodId, prod.seller,category, prod.productPrice, prod.bidCounter);
+    } else {
+        printf("+ Error: Delete not possible");
+    }
+}
+
+void processCommand(char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4, tList *L) {
+
+    printf("********************\n");
+    tProductCategory category;
     switch (command) {
         case 'N':
-            printf("Command: %s %c %s %s %s %s\n", commandNumber, command, param1, param2, param3, param4);
+            printf("%s %c: product %s seller %s category %s price %s\n", commandNumber, command, param1, param2, param3, param4);
+            if(strcmp(param3, "book") == 0) {
+                category = book;
+            } else {
+                category = painting;
+            }
+            new(param1, param2, category, atof(param4), L);
+
             break;
         case 'S':
             break;
         case 'B':
             break;
         case 'D':
+            printf("%s %c: product %s seller %s category %s price %s\n", commandNumber, command, param1, param2, param3, param4);
             break;
         default:
             break;
@@ -77,9 +133,9 @@ int main(int nargs, char **args) {
     if (nargs > 1) {
         file_name = args[1];
     } else {
-        #ifdef INPUT_FILE
+#ifdef INPUT_FILE
         file_name = INPUT_FILE;
-        #endif
+#endif
     }
 
     readTasks(file_name);
